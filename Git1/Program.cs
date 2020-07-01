7fb58dd8a7d40e93b13d7b1938772e2e8e782726
AnaKing39 <anuchin@gmail.com>
2020-07-01 12:45:26 +0700

    First commiting

--- Git1/Program.cs
+++ Git1/Program.cs
@@ -7,6 +7,7 @@ namespace Git1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub");
         }
     }
 }
